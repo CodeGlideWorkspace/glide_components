d1c728dd6c1cf3a8699025ea93b8d497b55dc436
guifu.wu <guifu.wu@dmall.com>
2023-11-05 15:19:09 +0800

    chore(depend): 添加第三方依赖

--- .doerrc.js
+++ .doerrc.js
@@ -12,11 +12,7 @@ module.exports = {
   // 开启BrowserRouter模式
   browserHistory: false,
 
-  plugins: [
-  
-  '@doerjs/plugin-less',
-  
-  ],
+  plugins: ['@doerjs/plugin-less'],
 
   // 自定义<Suspense fallback={<Loading />}> loading组件
   // 布局加载和页面加载公用一个loading
