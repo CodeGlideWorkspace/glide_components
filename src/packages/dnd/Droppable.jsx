86e933f71e103d83bb5913a7da3d2a9de44ea156
louis <dong.luo@dmall.com>
2023-12-02 23:20:41 +0800

    feat(demo): 开发中

--- src/packages/dnd/Droppable.jsx
+++ src/packages/dnd/Droppable.jsx
@@ -2,11 +2,52 @@ import React from 'react'
 import { useDrop } from 'react-dnd'
 
 import styles from './Droppable.module.css'
-function Droppable({ children, onDrop, style, accept }) {
-  const [{ isOver, canDrop }, drop] = useDrop({
+
+// 枚举 drop 在容器的位置 外部的 左右上下
+
+function calcPosition(monitor, dropRef, depth) {
+  const clientOffset = monitor.getClientOffset()
+  const containerRect = dropRef.current.getBoundingClientRect()
+
+  // 计算相对于容器的位置
+  const x = clientOffset.x - containerRect.left
+  const y = clientOffset.y - containerRect.top
+
+  // 定义插槽的高度和宽度
+  const slotWidth = containerRect.width / 3
+  const slotHeight = containerRect.height / 3
+  let position = ''
+  // 判断放置的位置
+  if (y < slotHeight) {
+    // 放置在上方插槽
+    console.log(`outer-top-${depth}`)
+    position = 'top'
+  } else if (y > 2 * slotHeight) {
+    // 放置在下方插槽
+    console.log(`outer-bottom-${depth}`)
+    position = 'bottom'
+  } else if (x < slotWidth) {
+    // 放置在左侧插槽
+    console.log(`outer-left-${depth}`)
+    position = 'left'
+  } else if (x > 2 * slotWidth) {
+    // 放置在右侧插槽
+    console.log(`outer-right-${depth}`)
+    position = 'right'
+  } else {
+    // 放置在中间插槽
+    console.log(`outer-center-${depth}`)
+    position = 'center'
+  }
+  return position
+}
+
+function Droppable({ children, onDrop, state = {}, depth, style, accept }) {
+  const [{ isOver, canDrop, dropPosition }, drop] = useDrop({
     accept,
     drop(item, monitor) {
-      onDrop(item, monitor)
+      state.position = calcPosition(monitor, dropRef, depth)
+      onDrop(item, monitor, state)
     },
     // accept(item, monitor) {
     //     onDropAccepted(item, monitor)
@@ -35,20 +76,31 @@ function Droppable({ children, onDrop, style, accept }) {
     // dragOverCapture(item, monitor) {
     //     onDragOverCapture(item, monitor)
     // },
-    collect: (monitor) => ({
-      isOver: monitor.isOver(),
-      canDrop: monitor.canDrop(),
-    }),
+    collect: (monitor) => {
+      const isOver = monitor.isOver()
+      const dropPosition = () => {
+        if (!isOver) {
+          return ''
+        }
+        const position = calcPosition(monitor, dropRef, depth)
+        return `插入位置：${position}`
+      }
+      return { isOver, canDrop: monitor.canDrop(), dropPosition: dropPosition() }
+    },
   })
   const isActive = isOver && canDrop
-  let backgroundColor = '#222'
+  let backgroundColor = 'rgba(0,0,0,0.3)'
   if (isActive) {
-    backgroundColor = '#333'
+    backgroundColor = 'rgba(0,0,0,0.6)'
   } else if (canDrop) {
-    backgroundColor = '#444'
+    backgroundColor = 'rgba(0,0,0,0.4)'
   }
+  const dropRef = React.useRef(null)
+  drop(dropRef)
+
   return (
-    <div ref={drop} style={{ ...style, backgroundColor }} className={styles.droppable}>
+    <div ref={dropRef} style={{ ...style, backgroundColor }} className={styles.droppable}>
+      {dropPosition}
       {children}
     </div>
   )
--- src/pages/index/Index.page.jsx
+++ src/pages/index/Index.page.jsx
@@ -27,8 +27,8 @@ export default function Index() {
   const handleDrag = (item, monitor) => {
     console.log(item, monitor)
   }
-  const handleDrop = (item, monitor) => {
-    console.log(item, monitor)
+  const handleDrop = (item, monitor, state) => {
+    console.log(item, monitor, state)
     if (!item.id) {
       item.id = genUUID()
     }
@@ -37,40 +37,115 @@ export default function Index() {
       return [...box2, item]
     })
   }
+  const handleDropInner = (item, monitor, state) => {
+    console.log(item, monitor, state)
+    if (!item.id) {
+      item.id = genUUID()
+    }
+
+    setBox2((box2) => {
+      return box2.map((box) => {
+        if (box.id === state.id) {
+          // 根据 position 插入
+          // 当插入为 left 或者 right 时，需要将当前的col 要span 为 仅一个时为24 两个时为12 三个时为8
+          if (state.position === 'left') {
+            const len = box.slots?.length || 1
+            box.slots = [item, ...(box.slots || [])].map((slot) => {
+              return {
+                ...slot,
+                span: 24 / (len + 1),
+              }
+            })
+          } else if (state.position === 'right') {
+            const len = box.slots?.length || 1
+            box.slots = [...(box.slots || []), item].map((slot) => {
+              return {
+                ...slot,
+                span: 24 / (len + 1),
+              }
+            })
+          } else if (state.position === 'top') {
+            box.slots = [item, ...(box.slots || [])]
+          } else if (state.position === 'bottom') {
+            box.slots = [...(box.slots || []), item]
+          } else {
+            // 默认下
+            box.slots = [...(box.slots || []), item]
+          }
+        }
+        return box
+      })
+    })
+  }
+  const addMore = () => {
+    return <div style={{ minHeight: '50px', borderWidth: '1px', borderStyle: 'dashed' }}>请拖入组件</div>
+  }
+
   const emptyBox = () => {
-    return <div style={{ minHeight: '20px' }}>请拖入组件</div>
+    return <div style={{ minHeight: '100px' }}>请拖入[module]组件</div>
   }
   const rowBox = ({ children, type, id }) => {
     const hasChildren = children && children.length > 0
     return (
       <Row key={id}>
-        <Col span={24}>{!hasChildren ? emptyBox() : children}</Col>
+        <Col span={24}>
+          <Droppable onDrop={handleDropInner} state={{ type, id }} depth={2} accept={['module']}>
+            {!hasChildren ? emptyBox() : children}
+          </Droppable>
+        </Col>
       </Row>
     )
   }
   const moduleBox = ({ name, id, children }) => {
     return (
-      <Module title="1111" key={id}>
-        {name}
+      <Module title={name} key={id}>
+        {id}
         {children}
       </Module>
     )
   }
+  const column3Box = ({ children, type, id }) => {
+    const hasChildren = children && children.length > 0
+    return (
+      <Row key={id}>
+        <Col span={8}>
+          <Droppable onDrop={handleDropInner} state={{ type, id }} depth={2} accept={['module']}>
+            {!hasChildren ? emptyBox() : children[0]}
+          </Droppable>
+        </Col>
+        <Col span={8}>
+          <Droppable onDrop={handleDropInner} state={{ type, id }} depth={2} accept={['module']}>
+            {!hasChildren || !children[1] ? emptyBox() : children[1]}
+          </Droppable>
+        </Col>
+        <Col span={8}>
+          <Droppable onDrop={handleDropInner} state={{ type, id }} depth={2} accept={['module']}>
+            {!hasChildren || !children[2] ? emptyBox() : children[2]}
+          </Droppable>
+        </Col>
+      </Row>
+    )
+  }
+
   const renderDropContent = (item) => {
     const { type, slots } = item
     let children = []
     if (slots && slots.length > 0) {
       children = slots.map((child) => renderDropContent(child))
     }
+    console.log(children)
     if (type === 'module') {
       return moduleBox({ ...item, children })
     }
     if (type === 'row') {
       return rowBox({ ...item, children })
     }
+    if (type === 'column3') {
+      return column3Box({ ...item, children })
+    }
     return emptyBox()
   }
-
+  console.log('box2', box2)
   return (
     <div className={styles.index}>
       <DndContainer>
@@ -78,22 +153,28 @@ export default function Index() {
           <Col span={6}>
             <Module title="library">
               <Module>
+                <Draggable onDrag={handleDrag} type="row" name="row1">
+                  <Button type="primary">Row</Button>
+                </Draggable>
+              </Module>
+              <Module>
                 <Draggable onDrag={handleDrag} type="module" name="module1">
                   <Button type="primary">Module</Button>
                 </Draggable>
               </Module>
               <Module>
-                <Draggable onDrag={handleDrag} type="row" name="row1">
-                  <Button type="primary">Row</Button>
+                <Draggable onDrag={handleDrag} type="column3" name="column3">
+                  <Button type="primary">colum3</Button>
                 </Draggable>
               </Module>
             </Module>
           </Col>
           <Col span={12}>
-            <Droppable onDrop={handleDrop} accept={['module', 'row']}>
+            <Droppable onDrop={handleDrop} depth={1} accept={['row', 'column3']}>
               {box2.map((item) => {
                 return renderDropContent(item)
               })}
+              {addMore()}
             </Droppable>
           </Col>
           <Col span={6}>
